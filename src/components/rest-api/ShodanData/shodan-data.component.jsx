37a6e96e2596b944d89fd1c3de476978896a5d28
JORGE REYES <jorge.r@weareshellshock.com>
2021-08-05 15:19:18 -0500

    Se llega hasta los datos buscados con el algoritmo encontrarVulns

--- src/components/rest-api/ShodanData/shodan-data.component.jsx
+++ src/components/rest-api/ShodanData/shodan-data.component.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const ShodanData = () => {
   const [vuln, setVuln] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     getData();
@@ -16,28 +17,52 @@ const ShodanData = () => {
     //https://api.shodan.io/shodan/host/192.188.58.61?key=FL5f6aSOu464esmyqf7c0kDDi0UycPNN
     const data = await (await fetch(url)).json();
     console.log(data);
+    console.log(data.data.length);
     setVuln(data);
+    const local = encontrarVulns(data.data, data.vulns);
+    console.log(local);
+    setData(local);
+    // local.map((item1) => console.log(item1.key));
+    // console.log(data.vulns.length);
+    // data.vulns.map((item) => console.log(item));
   };
+
+  function encontrarVulns(data, vulns) {
+    var val = [];
+    for (let i = 0; i < data.length; i++) {
+      if (typeof data[i].vulns != "undefined") {
+        val.push(data[i].vulns);
+      }
+    }
+    // console.log(Object.keys(val[0]));
+
+    for (let k = 0; k < vulns.length; k++) {
+      console.log(vulns[k]);
+      for (let j = 0; j < val.length; j++) {
+        console.log(val[j][vulns[k]]);
+      }
+    }
+
+    // for (let j = 0; j < val.length; j++) {
+    //   console.log(Object.keys(val[j]));
+    //   for(let k=0;k<vulns.length;k++){
+    //     console.log(k-vulns[k]);
+    //
+    //     }
+    //   }
+    // }
+    // console.log(val);
+    // console.log(val.length);
+    return val;
+  }
 
   return (
     <div>
       <h1>SH API CONSUMER</h1>
-      <h2>
-        {vuln.org}-{vuln.ip_str}-{vuln.isp}-[{vuln.last_update}]
-      </h2>
 
       {typeof vuln.vulns != "undefined" ? (
         <>
           <p>ESTA IP TIENE VULNERABILIDADES</p>
-          {/*{vuln.vulns.map((item) =>*/}
-          {/*  vuln.data.map((x) => console.log(x.vulns[item].cvss))*/}
-          {/*)}*/}
-          {vuln.data.map((x) =>
-            vuln.vulns.map((item) =>
-              console.log(vuln.data[1].vulns["CVE-2014-0117"].references)
-            )
-          )}
-          {/*<p>{vuln.data.map((item) => console.log(item.vulns))}</p>*/}
         </>
       ) : (
         <p>ESTA IP NO TIENE VULNERABILIDADES</p>
@@ -46,38 +71,3 @@ const ShodanData = () => {
   );
 };
 export default ShodanData;
-// data[1].vulns["CVE-2014-0117"].references;
-// data[1].vulns
-// data[1].vulns["CVE-2014-0117"];
-// data[1].vulns['CVE-2014-0117'].cvss
-//console.log(`${item}-${vuln.data[1].vulns[item].cvss}`)
-{
-  /*{vuln.vulns.map((item, index) => (*/
-}
-{
-  /*  <li key={index}>{item}</li>*/
-}
-{
-  /*))}*/
-}
-// {vuln.vulns.map((item, index) =>
-//     vuln.data.map((x) => console.log(`${item}-${x.vulns[item].cvss}`))
-// )}
-{
-  /*{*/
-}
-{
-  /*  // vuln.data.map((item, index) => console.log(item.vulns))*/
-}
-{
-  /*  vuln.vulns.map((item) =>*/
-}
-{
-  /*    vuln.data.map((item1) => console.log(item1.vulns[item].cvss))*/
-}
-{
-  /*  )*/
-}
-{
-  /*}*/
-}
