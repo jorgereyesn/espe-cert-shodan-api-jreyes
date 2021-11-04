856b27e443e2a63ccee7f13863d138c8e6452f39
JORGE REYES <jorge.r@weareshellshock.com>
2021-11-04 00:47:43 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import MainPageComponent from "./Pages/MainPage/main-page.component";
 import Header from "./components/header/header.component";
+import Footer from "./components/footer/footer.component";
 
 function App() {
   return (
     <div>
       <Header />
       <MainPageComponent />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import * as S from "./footer.styles";
+import Logo from "../../images/espe.png";
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.Wrapper>
+        <S.Content>
+          <S.Image src={Logo} />
+          <S.Description>
+            <strong>PROJECT NAME.</strong> <br />
+            Design and Implementation of the IT infrastructure and service
+            management system for the ESPE Academic CERT
+          </S.Description>
+          <S.Description>
+            <strong>© 2021 PIC-2020-ESPE-CERT.</strong> <br />
+            Todos los derechos reservados.
+          </S.Description>
+        </S.Content>
+      </S.Wrapper>
+    </S.Footer>
+  );
+};
+
+export default Footer;
